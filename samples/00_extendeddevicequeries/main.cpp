11b9eb89b4ff2de72bd722d2d8329b83c39fdae1
Ben Ashbaugh <ben.ashbaugh@intel.com>
2023-08-15 08:02:04 -0700

    add a few more extended device queries

--- samples/00_extendeddevicequeries/main.cpp
+++ samples/00_extendeddevicequeries/main.cpp
@@ -18,36 +18,50 @@ int main()
         for( auto& d : p.get_devices() )
         {
             std::cout << "SYCL Device: " << d.get_info<info::device::name>() << std::endl;
-            printf("\tVendor ID:       %04X\n", d.get_info<info::device::vendor_id>());
+            printf("\tVendor ID:         %04X\n", d.get_info<info::device::vendor_id>());
 #if SYCL_EXT_INTEL_DEVICE_INFO >= 1
             if (d.has(aspect::ext_intel_device_id)) {
-                printf("\tDevice ID:       %04X\n", d.get_info<ext::intel::info::device::device_id>());
+                printf("\tDevice ID:         %04X\n", d.get_info<ext::intel::info::device::device_id>());
             }
 #endif
-            printf("\tMax Sub-Devices: %u\n", d.get_info<info::device::partition_max_sub_devices>());
+            printf("\tMax Sub-Devices:   %u\n", d.get_info<info::device::partition_max_sub_devices>());
+            printf("\tMax Compute Units: %u\n", d.get_info<info::device::max_compute_units>());
 #if SYCL_EXT_INTEL_DEVICE_INFO >= 3
             if (d.has(aspect::ext_intel_gpu_slices)) {
-                printf("\tNum Slices:      %u\n", d.get_info<ext::intel::info::device::gpu_slices>());
+                printf("\tNum Slices:        %u\n", d.get_info<ext::intel::info::device::gpu_slices>());
             }
             if (d.has(aspect::ext_intel_gpu_slices) &&
                 d.has(aspect::ext_intel_gpu_subslices_per_slice)) {
-                printf("\tNum Sub-Slices:  %u\n",
+                printf("\tNum Sub-Slices:    %u\n",
                     d.get_info<ext::intel::info::device::gpu_slices>() *
                     d.get_info<ext::intel::info::device::gpu_subslices_per_slice>());
             }
-            if (d.has(aspect::ext_intel_gpu_slices) && 
+            if (d.has(aspect::ext_intel_gpu_slices) &&
                 d.has(aspect::ext_intel_gpu_subslices_per_slice) &&
                 d.has(aspect::ext_intel_gpu_eu_count_per_subslice)) {
-                printf("\tNum EUs:         %u\n",
+                printf("\tNum EUs (indirect):%u\n",
                     d.get_info<ext::intel::info::device::gpu_slices>() *
                     d.get_info<ext::intel::info::device::gpu_subslices_per_slice>() *
                     d.get_info<ext::intel::info::device::gpu_eu_count_per_subslice>());
             }
 #endif
+#if SYCL_EXT_INTEL_DEVICE_INFO >= 1
+            if (d.has(aspect::ext_intel_gpu_eu_count)) {
+                printf("\tNum EUs (direct):  %u\n",
+                    d.get_info<ext::intel::info::device::gpu_eu_count>());
+            }
+            if (d.has(aspect::ext_intel_pci_address)) {
+                auto pciAddress = d.get_info<ext::intel::info::device::pci_address>();
+                printf("\tPCI Address:       %s\n", pciAddress.c_str());
+            }
+#endif
 #if SYCL_EXT_INTEL_DEVICE_INFO >= 2
             if (d.has(aspect::ext_intel_device_info_uuid)) {
-                auto deviceUUID = d.get_info<ext::intel::info::device::uuid>();
-                printf("\tDevice UUID:     %02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X\n",
+                std::array<unsigned char, 16> deviceUUID;
+                deviceUUID.fill(0xCC);
+
+                deviceUUID = d.get_info<ext::intel::info::device::uuid>();
+                printf("\tDevice UUID:       %02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X\n",
                     deviceUUID[0], deviceUUID[1], deviceUUID[2], deviceUUID[3],
                     deviceUUID[4], deviceUUID[5], deviceUUID[6], deviceUUID[7],
                     deviceUUID[8], deviceUUID[9], deviceUUID[10], deviceUUID[11],
