5333ed01f6c19c94425c71cf5d733a575a4ade5b
Ben Ashbaugh <ben.ashbaugh@intel.com>
2023-09-14 12:40:08 -0700

    add another partial reduction example

--- samples/99_loadperf/main.cpp
+++ samples/99_loadperf/main.cpp
@@ -11,6 +11,7 @@
 #include <chrono>
 #include <iostream>
 #include <numeric>
+#include <random>
 #include <vector>
 
 using test_clock = std::chrono::steady_clock;
@@ -108,9 +109,32 @@ private:
     int* src;
 };
 
+class PartialReductionCoalescedx4 {
+public:
+    PartialReductionCoalescedx4(int* dst_, int* src_) : dst(dst_), src(src_) {}
+    void operator() [[intel::kernel_args_restrict]] (sycl::nd_item<1> item) const {
+        auto srcid = item.get_group(0) * item.get_local_range(0) * per_wi + item.get_local_id(0) * 4;
+        int sum = 0;
+
+        #pragma unroll
+        for (size_t i = 0; i < per_wi / 4; i++) {
+            sum += src[srcid + i * item.get_local_range(0) * 4 + 0];
+            sum += src[srcid + i * item.get_local_range(0) * 4 + 1];
+            sum += src[srcid + i * item.get_local_range(0) * 4 + 2];
+            sum += src[srcid + i * item.get_local_range(0) * 4 + 3];
+        }
+
+        auto dstid = item.get_group(0) * item.get_local_range(0) + item.get_local_id(0);
+        dst[dstid] = sum;
+    }
+private:
+    int* dst;
+    int* src;
+};
+
 bool checkPartialReductionResults(const std::vector<int>& dst, const std::vector<int>& src) {
-    int src_result = 0; for (auto v : src) { src_result += v; }
-    int dst_result = 0; for (auto v : dst) { dst_result += v; };
+    int src_result = 0; std::for_each(src.cbegin(), src.end(), [&](int n){ src_result += n; });
+    int dst_result = 0; std::for_each(dst.cbegin(), dst.end(), [&](int n){ dst_result += n; });
 
     if (src_result != dst_result) {
         std::cout << "Full reduction: " << src_result << ", check: " << dst_result << "\n";
@@ -125,7 +149,14 @@ int main(int, char**) {
     std::cout << "Running on device: " << q.get_device().get_info<sycl::info::device::name>() << "\n";
 
     std::vector<int> h_src(num_wis * per_wi);
+#if 0
     std::iota(h_src.begin(), h_src.end(), 0);
+#else
+    std::random_device rdev;
+    std::mt19937 rng(rdev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0, 255);
+    std::generate(h_src.begin(), h_src.end(), [&]() { return dist(rng); });
+#endif
 
     int* src = sycl::malloc_device<int>(h_src.size(), q);
     q.copy(h_src.data(), src, h_src.size()).wait();
@@ -202,6 +233,24 @@ int main(int, char**) {
         checkPartialReductionResults(h_dst, h_src);
         auto gbps = num_wis * per_wi * sizeof(int) / best / 1024 / 1024 / 1024;
         std::cout << "Finished coalesced partial reduction in " << best << " seconds (" << gbps << " GB/s).\n";
+    }
+
+    {
+        float best = 999.0f;
+        for (size_t i = 0; i < iterations; i++) {
+            q.fill(dst, 0, h_src.size()).wait();
+
+            auto start = test_clock::now();
+            q.parallel_for(sycl::nd_range<1>{num_wis, 256}, PartialReductionCoalescedx4(dst, src)).wait();
+            auto end = test_clock::now();
+            std::chrono::duration<float> elapsed_seconds = end - start;
+            best = std::min(best, elapsed_seconds.count());
+        }
+        std::vector<int> h_dst(num_wis);
+        q.copy(dst, h_dst.data(), h_dst.size()).wait();
+        checkPartialReductionResults(h_dst, h_src);
+        auto gbps = num_wis * per_wi * sizeof(int) / best / 1024 / 1024 / 1024;
+        std::cout << "Finished coalesced partial reduction x4 in " << best << " seconds (" << gbps << " GB/s).\n";
     }
 
     sycl::free(src, q);
