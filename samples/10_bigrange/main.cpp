9f07f322313b5f7d18691364443295bdf871458b
Ben Ashbaugh <ben.ashbaugh@intel.com>
2026-02-19 15:42:12 -0800

    catch exceptions

--- samples/10_bigrange/main.cpp
+++ samples/10_bigrange/main.cpp
@@ -7,6 +7,16 @@
 #include <sycl/sycl.hpp>
 #include <popl/popl.hpp>
 
+auto handle_async_error = [](sycl::exception_list elist) {
+    for (auto &e : elist) {
+        try {
+            std::rethrow_exception(e);
+        } catch (...) {
+            std::cout << "Caught async SYCL exception!!\n";
+        }
+    }
+};
+
 int main(int argc, char** argv)
 {
     int platformIndex = 0;
@@ -39,66 +49,75 @@ int main(int argc, char** argv)
         }
     }
 
-    sycl::platform platform = sycl::platform::get_platforms()[platformIndex];
-    printf("Running on SYCL platform: %s\n", platform.get_info<sycl::info::platform::name>().c_str());
+    try
+    {
+        sycl::platform platform = sycl::platform::get_platforms()[platformIndex];
+        printf("Running on SYCL platform: %s\n", platform.get_info<sycl::info::platform::name>().c_str());
 
-    sycl::device device = platform.get_devices()[deviceIndex];
-    printf("Running on SYCL device: %s\n", device.get_info<sycl::info::device::name>().c_str());
+        sycl::device device = platform.get_devices()[deviceIndex];
+        printf("Running on SYCL device: %s\n", device.get_info<sycl::info::device::name>().c_str());
 
-    sycl::context context = sycl::context{ device };
-    sycl::queue queue = sycl::queue{ context, device, sycl::property::queue::in_order() };
+        sycl::context context = sycl::context{ device };
+        sycl::queue queue = sycl::queue{ context, device, handle_async_error, sycl::property::queue::in_order() };
 
-    printf("Allocating and initializing memory...\n");
+        printf("Allocating and initializing memory...\n");
 
-    const size_t sz = gwx / divisor;
+        const size_t sz = gwx / divisor;
 
-    int* pDevTotal = sycl::malloc_device<int>(1, queue);
-    int* pDevCheck = sycl::malloc_device<int>(sz, queue);
+        int* pDevTotal = sycl::malloc_device<int>(1, queue);
+        int* pDevCheck = sycl::malloc_device<int>(sz, queue);
 
-    queue.fill(pDevTotal, 0, 1).wait();
-    queue.fill(pDevCheck, 0, sz).wait();
+        queue.fill(pDevTotal, 0, 1).wait_and_throw();
+        queue.fill(pDevCheck, 0, sz).wait_and_throw();
 
-    printf("Launching a kernel with global work size %zu and local work size %zu...\n", gwx, lwx);
+        printf("Launching a kernel with global work size %zu and local work size %zu...\n", gwx, lwx);
 
-    queue.parallel_for(sycl::nd_range<1>{gwx, lwx}, [=](sycl::nd_item<1> item) {
-        const size_t global_id = item.get_global_id(0);
-        if (global_id % divisor == 0) {
-            sycl::atomic_ref<int, sycl::memory_order::relaxed,
-                sycl::memory_scope::device,
-                sycl::access::address_space::global_space> atomicTotal(pDevTotal[0]);
-            atomicTotal.fetch_add(1);
-            sycl::atomic_ref<int, sycl::memory_order::relaxed,
-                sycl::memory_scope::device,
-                sycl::access::address_space::global_space> atomicCheck(pDevCheck[global_id / divisor]);
-            atomicCheck.fetch_add(1);
+        queue.parallel_for(sycl::nd_range<1>{gwx, lwx}, [=](sycl::nd_item<1> item) {
+            const size_t global_id = item.get_global_id(0);
+            if (global_id % divisor == 0) {
+                sycl::atomic_ref<int, sycl::memory_order::relaxed,
+                    sycl::memory_scope::device,
+                    sycl::access::address_space::global_space> atomicTotal(pDevTotal[0]);
+                atomicTotal.fetch_add(1);
+                sycl::atomic_ref<int, sycl::memory_order::relaxed,
+                    sycl::memory_scope::device,
+                    sycl::access::address_space::global_space> atomicCheck(pDevCheck[global_id / divisor]);
+                atomicCheck.fetch_add(1);
+            }
+        }).wait_and_throw();
+
+        printf("Reading results...\n");
+
+        int hostTotal = 0;
+        std::vector<int> hostCheck(sz);
+        queue.copy(pDevTotal, &hostTotal, 1).wait();
+        queue.copy(pDevCheck, hostCheck.data(), sz).wait();
+
+        printf("total = %d (should be %zu)\n", hostTotal, sz);
+
+        bool match = true;
+        for (size_t i = 0; i < sz; i++) {
+            if (hostCheck[i] != 1) {
+                printf("check[%zu] = %d (should be 1)\n", i, hostCheck[i]);
+                match = false;
+            }
         }
-    }).wait();
 
-    printf("Reading results...\n");
-
-    int hostTotal = 0;
-    std::vector<int> hostCheck(sz);
-    queue.copy(pDevTotal, &hostTotal, 1).wait();
-    queue.copy(pDevCheck, hostCheck.data(), sz).wait();
-
-    printf("total = %d (should be %zu)\n", hostTotal, sz);
-
-    bool match = true;
-    for (size_t i = 0; i < sz; i++) {
-        if (hostCheck[i] != 1) {
-            printf("check[%zu] = %d (should be 1)\n", i, hostCheck[i]);
-            match = false;
+        if (match) {
+            printf("All checks passed.\n");
         }
+
+        printf("Freeing memory...\n");
+        sycl::free(pDevTotal, queue);
+        sycl::free(pDevCheck, queue);
+
+        printf("Done.\n");
+    }
+    catch (sycl::exception &e)
+    {
+        std::cout << "Caught SYCL exception: " << e.what() << "\n";
+        return 1;
     }
 
-    if (match) {
-        printf("All checks passed.\n");
-    }
-
-    printf("Freeing memory...\n");
-    sycl::free(pDevTotal, queue);
-    sycl::free(pDevCheck, queue);
-
-    printf("Done.\n");
     return 0;
 }
